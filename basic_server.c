29b8686d67e939b8957c158c548e311967580185
Jennifer Zhang <jennifer.newyork@aol.com>
2017-12-13 20:27:20 -0500

    finished assignment

--- basic_server.c
+++ basic_server.c
@@ -2,29 +2,39 @@
 #include <ctype.h>
 
 int main() {
-
   int to_client;
   int from_client;
-
+  
+  
   from_client = server_handshake( &to_client );
-  
-  char buf[BUFFER_SIZE];
-  //read text from client
-  read(from_client, buf, BUFFER_SIZE);
-  printf("read buf: %s\n", buf);
+  while(1){
+    char buf[BUFFER_SIZE];
+    //read text from client
+    read(from_client, buf, BUFFER_SIZE);
+    printf("read buf: %s\n", buf);
 
-  //change text to uppercase
-  int i = 0;
-  while (buf[i]) {
-    buf[i] = toupper(buf[i]);
-    i++;
-  }
-  printf("modified buf: %s\n", buf);
+    //checks to see if received message
+    if(strcmp(buf, "done!") == 0){
+      printf("server is done!\n");
+      exit(1);
+    }
+
+
+    //change text to uppercase
+    int i = 0;
+    while (buf[i]) {
+      buf[i] = toupper(buf[i]);
+      i++;
+    }
+    printf("modified buf: %s\n", buf);
   
-  //write text to client
-  int ret;
-  ret = write(to_client, buf, BUFFER_SIZE);
-  if (ret == -1)
-    printf("error writing\n");
-  printf("wrote buf: %s\n", buf);
+    //write text to client
+    int ret;
+    ret = write(to_client, buf, BUFFER_SIZE);
+    if (ret == -1)
+      printf("error writing\n");
+    printf("wrote buf: %s\n", buf);
+
+    
+  }
 }
--- client.c
+++ client.c
@@ -6,21 +6,36 @@ int main() {
   int to_server;
   int from_server;
 
-  from_server = client_handshake( &to_server );
-
-  char buf[BUFFER_SIZE];
-  //get text from stdin
-  printf("Input text:\n");
-  fgets(buf, BUFFER_SIZE, stdin);
-
-  //write text to server
-  write(to_server, buf, BUFFER_SIZE);
-  printf("wrote buf: %s\n", buf);
-
-  //read text from server
-  int ret;
-  ret = read(from_server, buf, BUFFER_SIZE);
-  printf("ret: %d\n", ret);
   
-  printf("read buf: %s\n", buf);
+  from_server = client_handshake( &to_server );
+  
+  while(1){
+    char buf[BUFFER_SIZE];
+    //get text from stdin
+    printf("Input text:\n");
+    fgets(buf, BUFFER_SIZE, stdin);
+
+    //checks to see if buf is q
+    if(strcmp(buf, "q\n") == 0){
+      write(to_server, "done!", BUFFER_SIZE);
+      printf("client is done!\n");
+      exit(1);
+    }
+
+
+
+    //write text to server
+    write(to_server, buf, BUFFER_SIZE);
+    printf("wrote buf: %s\n", buf);
+
+    //read text from server
+    int ret;
+    ret = read(from_server, buf, BUFFER_SIZE);
+    printf("ret: %d\n", ret);
+  
+    printf("read buf: %s\n", buf);
+  
+
+  }
+
 }
